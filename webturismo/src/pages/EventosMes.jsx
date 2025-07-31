f57b3a6c2acca320fbb676632176e38dee486b3c
thefishgluglu <danielfco.garciacuesta@gmail.com>
2025-07-31 10:39:39 +0200

    Eventos mes

--- webturismo/src/pages/EventosMes.jsx
+++ webturismo/src/pages/EventosMes.jsx
@@ -4,7 +4,7 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 import styles from "./EventosMes.module.css";
 
-const subcolecciones = ["castells", "santjoan", "fuegos", "bailes"];
+const subcolecciones = ["castells", "santjoan", "fuegos", "bailes","cavalcada","carnaval","temporada","semana","tarraco","santmagi","santatecla"];
 
 const EventosMes = () => {
   const { mes } = useParams();
